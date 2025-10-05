9449537e15498a0479da099021865c148039a81d
Victor Manuel Méndez Calderón <vicmendezc@hotmail.com>
2025-10-05 17:38:15 -0600

    Primer commit del proyecto

---
+++ src/utils/knowledge.js
@@ -0,0 +1,50 @@
+export const knowledge = [
+    {
+        name: 'HTML',
+    },
+    {
+        name: 'Javascript',
+    },
+    {
+        name: 'Java',
+    },
+    {
+        name: 'CSS',
+    },
+    {
+        name: 'PHP',
+    },
+    {
+        name: 'Tailwind css',
+    },
+    {
+        name: 'Typescript',
+    },
+    {
+        name: 'Astro',
+    },
+    {
+        name: 'React js',
+    },
+    {
+        name: 'React Native',
+    },
+    {
+        name: 'Node js',
+    },
+    {
+        name: 'API REST',
+    },
+    {
+        name: 'SQL',
+    },
+    {
+        name: 'Mongo DB',
+    },
+    {
+        name: 'Docker',
+    },
+    {
+        name: 'Express',
+    },
+]
---
+++ src/utils/projects.js
@@ -0,0 +1,44 @@
+export const projects = [
+    
+    {
+        name: "SICICC | JAZA Ingeniería",
+        description: "Colaboración para el sistema interno de administración SICICC de la empresa constructora JAZA Ingeniería.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/SICICC.png"
+    },
+    {
+        name: "Sitio web | Global Click México",
+        description: "Web principal para la empresa Global Click México, empresa de e-lerning.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/globalClickMexico.png"
+    },
+
+    {
+        name: "Click+",
+        description: "Plataforma de cursos en linea desarrollada para la empresa Global Click México, desarrollada con React js como frontend conectada a una API REST Node js con Typescript.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/Click%2B.png"
+    },
+
+    {
+        name: "Landing page | Curso Lectura Inteligente",
+        description: "Landing page para el curso de Lectura Inteligente de la empresa Global Click México.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/lecturaInteligente.png"
+    },
+
+    {
+        name: "Sitio Web | VELARIA",
+        description: "Sitio web de velas aromáticas artesanales.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/VELARIA.png"
+    },
+
+    {
+        name: "Landing page | Cornerstone Institute",
+        description: "Landing page para la escuela de idiomas Cornerstone Institute.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/Captura%20de%20pantalla%202025-10-05%20141020.png"
+    },
+
+    {
+        name: "API REST | Notificaciones por correo",
+        description: "API REST privada para notificaciones por correo electrónico con plntillas html personalizables.",
+        image: "https://qzvv9kr0sph7bvqi.public.blob.vercel-storage.com/emailAPI.png"
+    },
+    
+]
